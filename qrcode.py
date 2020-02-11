1f160165f84ae1d1305c2c0f5302b79f5853fc56
Satyam Kumar <33719556+Anonsatyam@users.noreply.github.com>
2020-02-11 19:05:29 +0530

    Comment Updated

--- qrcode.py
+++ qrcode.py
@@ -1,10 +1,14 @@
+#Downloads and Import the require modules which we are using for this project.
 import pyqrcode
 from pyqrcode import QRCode
 from pyzbar.pyzbar import decode
 from PIL import Image
 
+#Here in target put the link where you want your users to land after scanning your QR Code.
 target = "https://focused-lamport-ae0eba.netlify.com/"
 
 url = pyqrcode.create(target)
 
-url.svg("myqrc.jpeg", scale=8)
+#Here you can use the image formats like(svg, png, jpeg) according to your convenience.
+url.svg("myqrc.svg", scale=8)
+
